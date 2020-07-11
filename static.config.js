d4742e65b7cade719315f83a849469e677fc1e31
Florian Fehrenbacher <flo.fehrenbacher@icloud.com>
2020-07-11 21:31:53 +0200

    remove github-pages

--- static.config.js
+++ static.config.js
@@ -4,8 +4,6 @@ import path from 'path'
 // Typescript support in static.config.js is not yet supported, but is coming in a future update!
 
 export default {
-  siteRoot: 'https://flofehrenbacher.github.io',
-  basePath: 'gumbauern-alpakas',
   entry: path.join(__dirname, 'src', 'index.tsx'),
   plugins: [
     'react-static-plugin-typescript',
