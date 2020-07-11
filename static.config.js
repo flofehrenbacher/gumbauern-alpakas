055afd93cf66d1d0c90d0456a3b598bc67637e3d
Florian Fehrenbacher <flo.fehrenbacher@icloud.com>
2020-07-11 21:10:45 +0200

    fix deploy to github pages

--- static.config.js
+++ static.config.js
@@ -1,10 +1,11 @@
-import axios from 'axios'
 import path from 'path'
 // import { Post } from './types'
 
 // Typescript support in static.config.js is not yet supported, but is coming in a future update!
 
 export default {
+  siteRoot: 'https://flofehrenbacher.github.io',
+  basePath: 'gumbauern-alpakas',
   entry: path.join(__dirname, 'src', 'index.tsx'),
   plugins: [
     'react-static-plugin-typescript',
