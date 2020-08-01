7c73b7e59f113ee980842037fa0d328b62fca7af
Lukas Olbrich <38129155+LOlbrich@users.noreply.github.com>
2020-08-01 14:40:33 +0200

    Update alpakawanderung.tsx

--- src/pages/alpakawanderung.tsx
+++ src/pages/alpakawanderung.tsx
@@ -47,8 +47,8 @@ export default () => (
       schließt. Dazu gibt es Wissenswertes rund um ihre Herkunft und ihre
       Bedürfnisse. Anschließend werden die Tiere gehalftert und wir wandern im
       gemütlichen Alpaka-Tempo los. Unser Rundweg führt uns über Feldwege und je
-      nach Witterung in den Wald. Bei einem Zwischenstopp können nach Lust und
-      Laune Fotos gemacht werden. Zurück im Stall, lassen wir die Wanderung noch
+      nach Witterung in den Wald. Bei einem Zwischenstopp können nach Belieben
+      Fotos gemacht werden. Zurück im Stall, lassen wir die Wanderung noch
       gemütlich ausklingen und entlassen die Alpakas nach einer kleinen
       Belohnung auf die Weide.
     </p>
@@ -75,13 +75,16 @@ export default () => (
       Preise
     </h2>
     <ul className={internalStyles.list}>
-      <li>pro Alpakaführer 18,-- €</li>
-      <li>pro Begleitperson 10,--€</li>
+      <li>pro AlpakaführerIn 18€</li>
+      <li>pro Begleitperson 8€</li>
       <li>Kinder bis sechs Jahre sind frei</li>
-      <li>Kinder ab 14 Jahren dürfen alleine ein Tier führen</li>
+      <li>Kinder ab 12 Jahren dürfen alleine ein Tier führen</li>
       <li>
         Alpakas sind Herdentiere, deshalb sollte eine Wanderung mit mindestens
         zwei Tieren gebucht werden.
+      </li>
+      <li>
+        Gerne könnt Ihr auch einen Geschenkgutschein erwerben.
       </li>
     </ul>
     <h2
@@ -98,7 +101,10 @@ export default () => (
       </li>
       <li>
         Zum Wohl der Tiere behalten wir uns vor, den Termin bei Starkregen,
-        Gewitter, Sturm und Temperaturen ab 30° zu verschieben.
+        Gewitter, Sturm zu verschieben.
+      </li>
+      <li> 
+        Bei Temperaturen ab 26° finden die Wanderungen in den Morgen- oder Abendstunden statt
       </li>
       <li>Hunde dürfen uns leider nicht begleiten.</li>
     </ul>
