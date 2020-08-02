edecc00cf000690984a72872eb573bfed8a1a7df
Lukas Olbrich <38129155+LOlbrich@users.noreply.github.com>
2020-08-02 13:35:03 +0200

    Update ueber-alpakas.tsx

--- src/pages/ueber-alpakas.tsx
+++ src/pages/ueber-alpakas.tsx
@@ -11,7 +11,7 @@ export default () => (
       wobei ihre nächsten Verwandten die bekannten Lamas sind. Heute leben die
       meisten Alpakas immer noch in Peru, die Alpakazucht breitet sich aber auch
       auf anderen Kontinenten aus. Alpakas sind neugierige und sehr
-      soziale Tiere. Deshalb können sie nicht allein gehalten werden, sondern
+      soziale Tiere. Deshalb können sie nicht alleine gehalten werden, sondern
       müssen in einer Herde leben. Wenn Ihr mehr erfahren wollt, kommt doch einfach
       für eine Wanderung vorbei!
     </p>
