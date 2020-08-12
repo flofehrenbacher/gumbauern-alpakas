6019e6c1b6ce00fa0264c5e5fa4d93db4b094d2c
Florian Fehrenbacher <flo.fehrenbacher@icloud.com>
2020-08-12 20:30:27 +0200

    next try basePath

--- static.config.js
+++ static.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 export default {
   entry: path.join(__dirname, 'src', 'index.tsx'),
   siteRoot: 'https://gumbauern-alpakas.de',
-  basePath: 'https://gumbauern-alpakas.de',
+  basePath: '/',
   plugins: [
     'react-static-plugin-typescript',
     'react-static-plugin-emotion',
