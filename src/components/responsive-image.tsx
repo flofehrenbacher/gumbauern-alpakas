c4aafe67debdbeac32e76261c67f84e7c45cae74
Florian Fehrenbacher <flo.fehrenbacher@icloud.com>
2020-08-13 07:57:30 +0200

    fix: revert lazy loading for now

--- src/components/responsive-image.tsx
+++ src/components/responsive-image.tsx
@@ -1,24 +1,19 @@
+import { css } from 'emotion'
 import React, { ImgHTMLAttributes } from 'react'
-import { css, cx } from 'emotion'
-import lozad from 'lozad'
 
 type ResponsiveImageProps = {
   baseSrc: string
 } & ImgHTMLAttributes<HTMLImageElement>
 
 export function ResponsiveImage({ baseSrc, ...props }: ResponsiveImageProps) {
-  React.useEffect(() => {
-    const observer = lozad() // lazy loads elements with default selector as '.lozad'
-    observer.observe()
-  }, [])
-
   return (
-    <picture className={cx([styles, 'lozad'])} {...props}>
+    <picture className={styles} {...props}>
       <source
         type="image/webp"
         srcSet={`${baseSrc}_800.webp 800w, ${baseSrc}_1600.webp 1600w`}
       />
       <source type="image/jpeg" srcSet={`${baseSrc}_800.jpg`} />
+      <img src={`${baseSrc}_800.jpg`} {...props} />
     </picture>
   )
 }
