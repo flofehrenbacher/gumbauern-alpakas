d3edb995eb85b0c0b6b1d808bf48180f155a55e7
Lukas Olbrich <38129155+LOlbrich@users.noreply.github.com>
2020-11-01 18:27:34 +0100

    Update contact.tsx

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -38,9 +38,7 @@ export function Contact(
           </a>
         </span>
         <span className={styles.addressLine}>
-          Mobil (gerne per WhatsApp):
-        </span>
-        <span className={styles.addressLine}>
+          Mobil (WhatsApp):
           <a className={styles.phone} href="tel:+49 1575 4205232">
             01575 4205232
           </a>
