ec960069fae7a7b18107a29c1ca45a2bf169d879
Florian Fehrenbacher <florian.fehrenbacher@sz.de>
2021-08-06 12:37:22 +0200

    trace 10% of traffic with sentry for performance measurement

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -11,7 +11,7 @@ Sentry.init({
     SENTRY_DSN ||
     'https://6b00fda65ee849cca887727299f5f13b@o946651.ingest.sentry.io/5895697',
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 0,
+  tracesSampleRate: 0.1,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
