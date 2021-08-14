21ddf739b08602356a9da32aae47d175eec7ba98
Florian Fehrenbacher <flo.fehrenbacher@icloud.com>
2021-08-14 12:56:55 +0200

    add error-boundary for leaflet-map

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -2,32 +2,35 @@ import React from 'react'
 import { Icon, LatLngTuple } from 'leaflet'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import { css } from '@emotion/react'
+import { ErrorBoundary } from 'react-error-boundary'
 
 const position: LatLngTuple = [49.15636, 11.3629]
 
 export default function Map() {
   return (
-    <MapContainer center={position} zoom={13} css={styles.map}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <Marker
-        position={position}
-        icon={
-          new Icon({
-            iconUrl: '/img/location-marker.svg',
-            iconSize: [50, 50],
-          })
-        }
-      >
-        <Popup>
-          <a href="https://goo.gl/maps/xT88p5ppbEHf4EwYA">
-            Anfahrt über Google Maps
-          </a>
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <ErrorBoundary fallback={null}>
+      <MapContainer center={position} zoom={13} css={styles.map}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker
+          position={position}
+          icon={
+            new Icon({
+              iconUrl: '/img/location-marker.svg',
+              iconSize: [50, 50],
+            })
+          }
+        >
+          <Popup>
+            <a href="https://goo.gl/maps/xT88p5ppbEHf4EwYA">
+              Anfahrt über Google Maps
+            </a>
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </ErrorBoundary>
   )
 }
 
