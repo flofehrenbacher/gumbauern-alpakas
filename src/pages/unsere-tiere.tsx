8f25904aa49bb861049eb8c5a5f21205fdeb08e0
Lukas Olbrich <38129155+LOlbrich@users.noreply.github.com>
2024-05-30 06:36:46 +0200

    linebreaks for uschi and karl

--- src/pages/unsere-tiere.tsx
+++ src/pages/unsere-tiere.tsx
@@ -126,13 +126,13 @@ const alpakas: Alpaka[] = [
     nickname: 'Die Stolze',
     birthYear: 2011,
     description:
-      'Uschi, Grande Dame der Herde, zeigt den „jungen Wilden“ wo es lang geht. Sie führt die Wanderungen an und kann es absolut nicht leiden, von ihrem Gefolge überholt zu werden. Unsere liebe Uschi ist am 31.01.2023 im Stall bei ihrer Herde für immer eingeschlafen. Wir vermissen sie sehr!',
+      'Uschi, Grande Dame der Herde, zeigt den „jungen Wilden“ wo es lang geht. Sie führt die Wanderungen an und kann es absolut nicht leiden, von ihrem Gefolge überholt zu werden. <br /> Unsere liebe Uschi ist am 31.01.2023 im Stall bei ihrer Herde für immer eingeschlafen. Wir vermissen sie sehr!',
   },
   {
     name: 'Karl',
     nickname: 'Der Macho',
     birthYear: 2018,
     description:
-      'Karl muss immer der Erste sein, wenn es ums Fressen geht. Er macht gerne auf „dicke Hose“, aber wenn es ernst wird, versteckt er sich dann doch lieber hinter den Mädels. Unser Raufbold ist am 27.03.2023 umgezogen. Wir waren schon lange auf der Suche nach einer großen Herde, wo sich unserem Quertreiber auch mal jemand entgegenstellt. Zum Glück konnten wir unseren Karl guten Gewissens auf den Winklhof bringen. Einen besseren Platz hätten wir uns nicht vorstellen können, auch wenn es uns sehr schwer gefallen ist. Er hat sich super eingelebt und zeigt auch mal Respekt vor den stattlichen Lamas in seiner neuen Herde.',
+      'Karl muss immer der Erste sein, wenn es ums Fressen geht. Er macht gerne auf „dicke Hose“, aber wenn es ernst wird, versteckt er sich dann doch lieber hinter den Mädels. <br /> Unser Raufbold ist am 27.03.2023 umgezogen. Wir waren schon lange auf der Suche nach einer großen Herde, wo sich unserem Quertreiber auch mal jemand entgegenstellt. Zum Glück konnten wir unseren Karl guten Gewissens auf den Winklhof bringen. Einen besseren Platz hätten wir uns nicht vorstellen können, auch wenn es uns sehr schwer gefallen ist. Er hat sich super eingelebt und zeigt auch mal Respekt vor den stattlichen Lamas in seiner neuen Herde.',
   },
 ]
